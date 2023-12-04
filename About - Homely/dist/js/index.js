b18062d31613d0957f088748e191a3099e19edd0
Jhexter Valencia <143709592+jhexterr@users.noreply.github.com>
2023-12-04 12:19:10 +0800

    Delete About - Homely directory

--- About - Homely/dist/js/index.js
+++
@@ -1,6 +0,0 @@
-const headerNav = document.querySelector(".header-nav");
-const burgerBtn = document.querySelector(".header-burger");
-burgerBtn.addEventListener("click", () => {
-    headerNav.classList.toggle("open");
-    burgerBtn.classList.toggle("open");
-});
